79611d6455651830b61f14586738bb4c8b54f052
Ben Cavins <ben.cavins@flatironschool.com>
2024-01-08 15:22:33 -0500

    add fizzbuzz

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -9,9 +9,18 @@ def hows_the_weather(temperature):
     pass
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    # for n in range(0, num+1):
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return num
 
 def calculator(operation, num1, num2):
     # your code here
     pass
+
+fizzbuzz(20)
